a27f9c5c1f62041038143830920f06d63c75f764
elastic <elastic7327@gmail.com>
2017-12-04 00:31:31 +0900

    update

--- src/base.py
+++ src/base.py
@@ -7,7 +7,9 @@ import pprint
 class BaseTests(unittest.TestCase):
 
     def setUp(self):
-        self.info = "https://api.bithumb.com/public/ticker/"
+        self.ticker_url = "https://api.bithumb.com/public/ticker/"
+        self.orderbook_url = "https://api.bithumb.com/public/orderbook/"
+        self.recent_transactions_url = "https://api.bithumb.com/public/recent_transactions/"
 
         self.W  = '\033[0m'  # white (normal)
         self.R  = '\033[31m' # red
--- src/test_public_api.py
+++ src/test_public_api.py
@@ -14,7 +14,7 @@ class TestStringMethods(BaseTests):
     @pytest.mark.skip(reason="skip it for a moment")
     def test_send_requests(self):
 
-        resp = requests.get(self.info + "ALL")
+        resp = requests.get(self.ticker_url + "BTC")
 
         assert resp.status_code == 200
 
@@ -27,7 +27,7 @@ class TestStringMethods(BaseTests):
         buffer = BytesIO()
         header_buffer = BytesIO()
         c = pycurl.Curl()
-        c.setopt(c.URL, self.info + "BTC")
+        c.setopt(c.URL, self.ticker_url + "BTC")
         c.setopt(c.WRITEDATA, buffer)
         c.setopt(c.HEADERFUNCTION, header_buffer.write)
 
@@ -51,3 +51,59 @@ class TestStringMethods(BaseTests):
         print(self.W)
         assert status_code == b"200"
 
+    def test_public_orderbook(self):
+        # https://api.bithumb.com/public/orderbook/{currency}
+        buffer = BytesIO()
+        header_buffer = BytesIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, self.orderbook_url + "BTC")
+        c.setopt(c.WRITEDATA, buffer)
+        c.setopt(c.HEADERFUNCTION, header_buffer.write)
+
+        c.perform()
+        c.close()
+
+        body = buffer.getvalue()
+        header = header_buffer.getvalue()
+        status_code = header[9:12]
+
+        #  TODO:  <26-11-17, Daniel> #
+        # need decoration!!!!!
+        # 데코레이션 하는게 나음
+        print(self.R)
+        pprint.pprint(json.loads(body))
+        print(self.W)
+        # pprint.pprint(json.loads(header))
+        print(self.G)
+        pprint.pprint(json.loads(status_code))
+        print(self.W)
+        assert status_code == b"200"
+
+    def test_recent_transactions(self):
+        # https://api.bithumb.com/public/recent_transactions/{currency}
+        buffer = BytesIO()
+        header_buffer = BytesIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, self.recent_transactions_url+ "BTC")
+        c.setopt(c.WRITEDATA, buffer)
+        c.setopt(c.HEADERFUNCTION, header_buffer.write)
+
+        c.perform()
+        c.close()
+
+        body = buffer.getvalue()
+        header = header_buffer.getvalue()
+        status_code = header[9:12]
+
+
+        #  TODO:  <26-11-17, Daniel> #
+        # need decoration!!!!!
+        # 데코레이션 하는게 나음
+        print(self.R)
+        pprint.pprint(json.loads(body))
+        print(self.W)
+        # pprint.pprint(json.loads(header))
+        print(self.G)
+        pprint.pprint(json.loads(status_code))
+        print(self.W)
+        assert status_code == b"200"
